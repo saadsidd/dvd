3f0187311ccdbeeb408f4c67f701479bf2711560
Saad Siddiq <siddiq.saad@gmail.com>
2022-10-15 23:35:59 -0600

    added logo bouncing animation

--- main.js
+++ main.js
@@ -2,19 +2,34 @@ const logo = document.getElementById('logo');
 const tvScreen = document.getElementById('screen');
 
 const centerLogo = function() {
-  let leftRandom = Math.round(Math.random() * tvScreen.offsetWidth);
-  if (leftRandom > logo.offsetWidth) {
-    leftRandom -= logo.offsetWidth;
+  let leftRandom = Math.floor(Math.random() * (tvScreen.offsetWidth - logo.offsetWidth + 1));
+  let topRandom = Math.floor(Math.random() * (tvScreen.offsetHeight - logo.offsetHeight + 1));
+
+  logo.style.left = leftRandom + 'px';
+  logo.style.top = topRandom + 'px';
+};
+
+const changeLogoColor = function() {
+  logo.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+};
+
+let x = 1;
+let y = -1;
+
+const animate = function() {
+  requestAnimationFrame(animate);
+
+  if (logo.offsetLeft <= 0 || logo.offsetLeft >= (tvScreen.offsetWidth - logo.offsetWidth)) {
+    changeLogoColor();
+    x *= -1;
+  }
+  if (logo.offsetTop <= 0 || logo.offsetTop >= (tvScreen.offsetHeight - logo.offsetHeight)) {
+    changeLogoColor();
+    y *= -1;
   }
 
-  let topRandom = Math.round(Math.random() * tvScreen.offsetHeight);
-  if (topRandom > logo.offsetHeight) {
-    topRandom -= logo.offsetHeight;
-  }
-  // logo.style.left = `${(tvScreen.offsetWidth / 2) - (logo.offsetWidth / 2)}px`;
-  logo.style.left = leftRandom + 'px';
-  // logo.style.top = `${(tvScreen.offsetHeight / 2) - (logo.offsetHeight / 2)}px`;
-  logo.style.top = topRandom + 'px';
+  logo.style.left = `${logo.offsetLeft += x}px`;
+  logo.style.top = `${logo.offsetTop += y}px`;
 };
 
 window.addEventListener('resize', () => {
@@ -22,3 +37,5 @@ window.addEventListener('resize', () => {
 });
 
 centerLogo();
+changeLogoColor();
+animate();
